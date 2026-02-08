c34e2f005a55e99713d8ef95c9f74ea5ae21fdd3
Tiniifan <30804632+Tiniifan@users.noreply.github.com>
2026-02-08 15:10:14 +0100

    Add MMMConfig and MMMAppear parser

---
+++ IEGOCheckpointMaker/MMMInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEGOCheckpointMaker
+{
+    public class MMMConfig
+    {
+        public int Id { get; set; }
+        public string Condition { get; set; }
+        public int StartIndex { get; set; }
+
+        public int Count { get; set; }
+    }
+
+    public class MMMAppear
+    {
+        public int Index { get; set; }
+        public int Type { get; set; }
+        public int PosX { get; set; }
+
+        public int PosY { get; set; }
+
+        public int Rot { get; set; }
+    }
+}
